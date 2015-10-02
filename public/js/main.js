e3995fa7482228b5f9ce57dacf01d40c14cd6be2
FrankFlitton <hello@frankflitton.com>
2015-10-02 11:49:29 -0400

    Nav Bar change test code

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#body');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#F89D0E');
+       }
+   });
+    }
+});
